61ac2c8c281167fc12f8ee60eff2d514aaac953c
koki-takishita <tkkokitk@yahoo.co.jp>
2021-02-25 12:33:58 +0900

    create rspec it users,user_sessions

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "UserSessions", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常' do
+      it 'ログイン処理が成功する'
+    end
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する'
+    end
+  end
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する'
+    end
+  end
 end
